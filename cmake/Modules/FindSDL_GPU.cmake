8e87cdec288251951f34ad4f1c1afa8314e63489
splintergu <splintergu@gmail.com>
2023-11-27 02:39:51 -0300

    fix static build sdl_gpu

--- cmake/Modules/FindSDL_GPU.cmake
+++ cmake/Modules/FindSDL_GPU.cmake
@@ -7,8 +7,15 @@ find_path(SDL_GPU_INCLUDE_DIR
 	PATH_SUFFIXES include include/SDL2 include/SDL_gpu SDL2 SDL_gpu
 	NO_CMAKE_FIND_ROOT_PATH NO_CMAKE_SYSTEM_PATH)
 
+if (LIBRARY_BUILD_TYPE STREQUAL STATIC)
+## todos los posibles nombres para los estáticos
+    SET(FIND_LIB_NAMES libSDL2_gpu.a libSDL2_gpu.dll.a libSDL2_gpu_s.a)
+else()
+    SET(FIND_LIB_NAMES libSDL2_gpu.so libSDL2_gpu.dll)
+endif()
+
 find_library(SDL_GPU_LIBRARY
-    NAMES SDL2_gpu libSDL2_gpu.dll.a libSDL2_gpu_s.a libSDL2_gpu.dll
+    NAMES ${FIND_LIB_NAMES}
     HINTS $ENV{SDL2GPUDIR}
     PATHS ../../vendor/sdl-gpu/build/$ENV{TARGET} ../../vendor/sdl-gpu/build/$ENV{TARGET}/SDL_gpu ../../vendor/sdl-gpu/build/$ENV{TARGET}/SDL_gpu/lib ../../vendor/sdl-gpu/build/$ENV{TARGET}/SDL_gpu/bin ../../vendor/sdl-gpu/build/$ENV{TARGET}/SDL_gpu$ENV{COMPILER} ../../vendor/sdl-gpu/build/$ENV{TARGET}/SDL_gpu$ENV{COMPILER}/lib ../../vendor/sdl-gpu/build/$ENV{TARGET}/SDL_gpu$ENV{COMPILER}/bin /usr/$ENV{TARGET} /usr/local/lib
     PATH_SUFFIXES lib lib64 bin
