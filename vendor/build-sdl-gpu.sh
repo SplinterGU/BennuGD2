b60e6d88adc9b6478ba1a61dd67690a13dde6fa6
splintergu <splintergu@gmail.com>
2023-12-02 19:54:50 -0300

    fix sdl-gpu windows native build

--- vendor/build-sdl-gpu.sh
+++ vendor/build-sdl-gpu.sh
@@ -13,7 +13,10 @@ do
     case $i in
         windows)
             TARGET=x86_64-w64-mingw32
-            CMAKE_EXTRA="-DCMAKE_TOOLCHAIN_FILE=../../cmake/Toolchains/Toolchain-cross-mingw32-linux.cmake -DSDL2_INCLUDE_DIR=/usr/x86_64-w64-mingw32/include/SDL2"
+            if [ "$MSYSTEM" != "MINGW64" ] && [ "$MSYSTEM" != "MINGW32" ]; then
+                # linux
+                CMAKE_EXTRA="-DCMAKE_TOOLCHAIN_FILE=../../cmake/Toolchains/Toolchain-cross-mingw32-linux.cmake -DSDL2_INCLUDE_DIR=/usr/x86_64-w64-mingw32/include/SDL2"
+            fi
             ;;
 
         linux)
@@ -22,7 +25,10 @@ do
 
         windows32)
             TARGET=i686-w64-mingw32
-            CMAKE_EXTRA="-DBUILD_WIN32=ON -DCMAKE_TOOLCHAIN_FILE=../../cmake/Toolchains/Toolchain-cross-mingw32-linux.cmake -DSDL2_INCLUDE_DIR=/usr/i686-w64-mingw32/include/SDL2 -DSDL2_LIBRARY=/usr/${TARGET}/bin/SDL2.dll -DSDL2_IMAGE_LIBRARY=/usr/${TARGET}/bin/SDL2_image.dll -DSDLMIXER_LIBRARY=/usr/${TARGET}/bin/SDL2_mixer.dll"
+            if [ "$MSYSTEM" != "MINGW64" ] && [ "$MSYSTEM" != "MINGW32" ]; then
+                # linux
+                CMAKE_EXTRA="-DBUILD_WIN32=ON -DCMAKE_TOOLCHAIN_FILE=../../cmake/Toolchains/Toolchain-cross-mingw32-linux.cmake -DSDL2_INCLUDE_DIR=/usr/i686-w64-mingw32/include/SDL2 -DSDL2_LIBRARY=/usr/${TARGET}/bin/SDL2.dll -DSDL2_IMAGE_LIBRARY=/usr/${TARGET}/bin/SDL2_image.dll -DSDLMIXER_LIBRARY=/usr/${TARGET}/bin/SDL2_mixer.dll"
+            fi
             ;;
 
         linux32)
@@ -67,23 +73,26 @@ fi
 export PKG_CONFIG_PATH
 export TARGET
 
-mkdir -p sdl-gpu/build/$TARGET 2>/dev/null
-
 echo "### Building SDL_gpu ($TARGET) ###"
 
-cd sdl-gpu/build/$TARGET
 if [ "$CLEAN" == "1" ]
 then
-    rm CMakeCache.txt
+    rm -rf sdl-gpu/build/$TARGET
 fi
+
+mkdir -p sdl-gpu/build/$TARGET 2>/dev/null
+
+cd sdl-gpu/build/$TARGET
+
 #cmake ../.. $DEBUG -DCMAKE_BUILD_TYPE=$BUILD_TYPE $CMAKE_EXTRA $VERBOSE -DSDL_gpu_BUILD_TESTS=YES -DSDL_gpu_BUILD_VIDEO_TEST=YES -DSDL_gpu_BUILD_TOOLS=YES
 #cmake ../.. $DEBUG -DCMAKE_BUILD_TYPE=$BUILD_TYPE $CMAKE_EXTRA $VERBOSE -DSDL_gpu_BUILD_TESTS=YES -DBUILD_TESTS=YES -DBUILD_VIDEO_TEST=YES -DBUILD_TOOLS=YES
 cmake ../.. $DEBUG -DINCLUDE_DIRECTORIES="${INCLUDE_DIRECTORIES}" -DCMAKE_BUILD_TYPE=$BUILD_TYPE $CMAKE_EXTRA $VERBOSE -DSDL_gpu_BUILD_TESTS=NO -DBUILD_TESTS=NO -DBUILD_VIDEO_TEST=NO -DBUILD_TOOLS=NO -DBUILD_DEMOS=NO -DBUILD_STATIC=YES
-if [ "$CLEAN" == "1" ]
-then
-    make clean
+if grep -q "CMAKE_GENERATOR:INTERNAL=Ninja" CMakeCache.txt; then
+    ninja
+elif grep -q "CMAKE_GENERATOR:INTERNAL=Unix Makefiles" CMakeCache.txt; then
+    make -j
 fi
-make -j
+
 if [ $? -eq 0 ]; then
     case $TARGET in
         x86_64-w64-mingw32)
