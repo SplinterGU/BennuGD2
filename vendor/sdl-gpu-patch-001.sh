bea4ededcdc3027dada439cc503f6979b1e55c1c
splintergu <splintergu@gmail.com>
2024-03-17 23:06:06 -0300

    - Removed obsoletes files

--- vendor/sdl-gpu-patch-001.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-patch -b sdl-gpu/src/renderer_GL_common.inl < sdl-gpu-odd-texture-fix.patch
--- vendor/sdl-gpu-patch-002.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-patch -b sdl-gpu/CMakeLists.txt < sdl-gpu-nintendo-switch.patch
