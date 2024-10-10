7de907b05fdba282ee8c6a17701f387b223456d6
splintergu <splintergu@gmail.com>
2024-10-10 12:32:10 -0300

    try fix github build windows32

--- modules/libmod_net/net.c
+++ modules/libmod_net/net.c
@@ -33,6 +33,9 @@
 #include <unistd.h>
 
 #ifdef _WIN32
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600  // Define Windows Vista o superior
+#endif
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
